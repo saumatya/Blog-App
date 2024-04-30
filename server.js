4bdb6a91115df3b27c4d1d6959b228a706bba7cc
saumatya <amatyasaurav3@gmail.com>
2024-04-30 23:38:23 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const app = express()
+
+app.set('view engine','ejs')
+
+app.use('/articles',articleRouter)
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test article',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test article2',
+        createdAt: new Date(),
+        description: 'Test description2'
+    }]
+    res.render('articles/index', {articles: articles})
+})
+app.listen(5000)
