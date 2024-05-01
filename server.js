e915ecfb68924700360b8b8b2e111219848760a2
saumatya <amatyasaurav3@gmail.com>
2024-05-01 08:08:35 +0300

    edit delete working

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+
 const app = express()
+// mongoose.connect('mongodb://localhost:27017/BlogDB')
+mongoose.connect('mongodb://localhost/blog')
+
+// mongoose
+// .connect('mongodb://localhost/blog')
+// .then(() => {
+//     console.log("DB connected successfully")
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`)
+//     })
+// })
+// .catch ((error) => console.log(error))
 
 app.set('view engine','ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+
+app.get('/', async (req, res) => {
+    const articles =await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index', {articles: articles})
+})
 
 app.use('/articles',articleRouter)
 
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test article',
-        createdAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title: 'Test article2',
-        createdAt: new Date(),
-        description: 'Test description2'
-    }]
-    res.render('articles/index', {articles: articles})
-})
 app.listen(5000)
---
+++ testMarked.js
@@ -0,0 +1,8 @@
+// Using require
+const { marked } = require('marked')
+
+// Using import (if your environment supports ES6)]
+//import marked from 'marked';
+
+
+console.log(marked('Hello Markdown!')); // Should output: <p>Hello Markdown!</p>
